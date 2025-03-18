d8560e6a37d2075810c072886f9152d8045a6709
LuciaGhezzi <s325951@studenti.polito.it>
2025-03-18 18:10:23 +0100

    adding lab01ex2

---
+++ dataset/transform_dataset.py
@@ -0,0 +1,20 @@
+import torchvision.transforms as transforms
+
+def transform():
+    # Define transformations for the dataset
+    transform = {
+        'train' : transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ]),
+        'val' : transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ]),
+    }
+    return transform
+
---
+++ utils/visualization.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from dataset.denormalize import *
+
+def visualization(dataloader_train):    
+    # Visualize one example for each class for 10 classes
+    fig, axes = plt.subplots(2, 5, figsize=(15, 6))
+    classes_sampled = []
+    found_classes = 0
+
+    for i, (inputs, classes) in enumerate(dataloader_train):
+    img = inputs[i].squeeze()
+    img = denormalize(img)
+    label = classes[i]
+    if label not in classes_sampled:
+        found_classes += 1
+        classes_sampled.append(label)
+        plt.subplot(2,5,found_classes)
+        plt.title(f'Class:{label}')
+        plt.imshow(img)
+    if found_classes == 10:
+        break
+
+    plt.show()
