36c359cd479f985f33f601de666a367a995336fd
LuciaGhezzi <s325951@studenti.polito.it>
2025-03-18 18:34:32 +0100

    modfication

--- dataset/denormalize.py
+++ dataset/denormalize.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Function to denormalize image for visualization
 def denormalize(image):
-    image = image.to('cpu').numpy()).transpose((1, 2, 0))
+    image = image.to('cpu').numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     image = image * std + mean
