d508863fa85f7411b40ebd77349672a8d4176380
LuciaGhezzi <s325951@studenti.polito.it>
2025-03-20 13:05:53 +0100

    update

--- dataset/transform_dataset.py
+++ dataset/transform_dataset.py
@@ -33,11 +33,11 @@ def transform_2():
 # lab 2 modification of dataset to create val set
 def modify_dataset():
     with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
-    for line in f:
-        fn, cls, *_ = line.split('\t')
-        os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+        for line in f:
+            fn, cls, *_ = line.split('\t')
+            os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
 
-        shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+            shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
 
     shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
 
